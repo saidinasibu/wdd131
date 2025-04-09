163f046779a92867f2959640ef6da08aaac43721
saidinasibu <adjinasibusaidi@gmail.com>
2025-04-09 13:56:46 -0700

    filtered temples update

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,128 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const currentYearSpan = document.getElementById("currentyear");
+    const lastModifiedSpan = document.getElementById("lastModified");
+
+    if (currentYearSpan) {
+        currentYearSpan.textContent = new Date().getFullYear();
+    }
+
+    if (lastModifiedSpan) {
+        lastModifiedSpan.textContent = document.lastModified;
+    }
+
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('nav');
+
+    hamburger.addEventListener('click', () => {
+        nav.classList.toggle('open');
+    });
+
+    const temples = [
+        {
+            templeName: "Aba Nigeria",
+            location: "Aba, Nigeria",
+            dedicated: "2005, August, 7",
+            area: 11500,
+            imageUrl:
+                "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+        },
+        {
+            templeName: "Manti Utah",
+            location: "Manti, Utah, United States",
+            dedicated: "1888, May, 21",
+            area: 74792,
+            imageUrl:
+                "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+        },
+        {
+            templeName: "Payson Utah",
+            location: "Payson, Utah, United States",
+            dedicated: "2015, June, 7",
+            area: 96630,
+            imageUrl:
+                "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+        },
+        {
+            templeName: "Yigo Guam",
+            location: "Yigo, Guam",
+            dedicated: "2020, May, 2",
+            area: 6861,
+            imageUrl:
+                "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+        },
+        {
+            templeName: "Washington D.C.",
+            location: "Kensington, Maryland, United States",
+            dedicated: "1974, November, 19",
+            area: 156558,
+            imageUrl:
+                "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+        },
+        {
+            templeName: "Lima Perú",
+            location: "Lima, Perú",
+            dedicated: "1986, January, 10",
+            area: 9600,
+            imageUrl:
+                "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+        },
+        {
+            templeName: "Mexico City Mexico",
+            location: "Mexico City, Mexico",
+            dedicated: "1983, December, 2",
+            area: 116642,
+            imageUrl:
+                "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+        },
+        {
+            templeName: "Caracas Venezuela Temple",
+            location: "Caracas, Venezuela",
+            dedicated: "2000, August 20",
+            area: 15332,
+            imageUrl:
+                "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/caracas-venezuela/400x225/caracas_venezuela_temple_detail_1691066_2400x1200.jpg"
+        },
+        {
+            templeName: "Ciudad Juárez Mexico Temple",
+            location: "Ciudad Juárez, Mexico",
+            dedicated: "2000, February 26",
+            area: 10700,
+            imageUrl:
+                "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/ciudad-juarez-mexico/400x250/ciudad-juarez-mexico-temple-lds-126122-wallpaper.jpg"
+        },
+    ];
+
+    // To create temple cards
+    function createTempleCard(temple) {
+        const figure = document.createElement('figure');
+        const img = document.createElement('img');
+        img.src = temple.imageUrl;
+        img.alt = temple.templeName;
+        img.loading = "lazy";
+
+        const figcaption = document.createElement('figcaption');
+        figcaption.innerHTML = `
+            <h3>${temple.templeName}</h3>
+            <p><strong>Location:</strong> ${temple.location}</p>
+            <p><strong>Dedicated:</strong> ${temple.dedicated}</p>
+            <p><strong>Area:</strong> ${temple.area} sq ft</p>
+        `;
+
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+
+        return figure;
+    }
+
+    // To display temple cards
+    function displayTemples(temples) {
+        const container = document.getElementById('temple-cards-container');
+        container.innerHTML = '';
+        temples.forEach(temple => {
+            const card = createTempleCard(temple);
+            container.appendChild(card);
+        });
+    }
+
+    displayTemples(temples);
+});
